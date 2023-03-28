f7f5aa4e37224fdf30f9675f48419c1b26a6b96d
pranavkelkar <kelkarpranav20.pk@gmail.com>
2023-03-28 09:36:42 -0700

    LeetCode: Two Sum

---
+++ src/Easy/TwoSum.java
@@ -0,0 +1,39 @@
+package Easy;
+
+/**
+ * This class solves two sum problem
+ *
+ */
+public class TwoSum {
+    /**
+     * Two sum helper function takes in the input array and the target value
+     *
+     * @param nums
+     * @param target
+     * @return int array with two indexes whoes sum of values is equal to the target
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        int[] finalArray = new int[2];
+        int curr = target - nums[0];
+        int i = 1;
+        while(i < nums.length)
+        {
+            int temp = i;
+            while(temp < nums.length)
+            {
+                if(curr == nums[temp])
+                {
+                    finalArray[0] = i-1;
+                    finalArray[1] = temp;
+                    return finalArray;
+                }
+                temp++;
+            }
+            curr = target - nums[i];
+            i++;
+        }
+        return finalArray;
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import Easy.TwoSum;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        /**
+         * Two sum
+         */
+        int[] finalArr = new int[2];
+        int[] input = {3,2,4};
+        finalArr = TwoSum.twoSum(input ,6);
+
+    }
+}
