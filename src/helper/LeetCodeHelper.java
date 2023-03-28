6d6129be1710bf68bdc8c4a47122ef72bc1a8125
pranavkelkar <kelkarpranav20.pk@gmail.com>
2023-03-28 14:52:35 -0700

    Adding a helper class to store all of the helper objects of LeetCode.

---
+++ src/helper/LeetCodeHelper.java
@@ -0,0 +1,16 @@
+package helper;
+
+/**
+ * Helper class for all the utility objects of LeetCode
+ *
+ */
+public final class LeetCodeHelper {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode() {};
+        ListNode(int val) { this.val = val;}
+        ListNode(int val, ListNode next) {this.val=val; this.next = next;}
+    }
+
+}
